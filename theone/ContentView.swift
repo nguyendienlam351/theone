817225403da04a5a7aa9edf018bfbe93ad278df9
nguyenlam <nguyenlam@nguyenlams-Mac.local>
2022-04-28 14:58:30 -0700

    first commit

---
+++ theone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  theone
+//
+//  Created by nguyenlam on 4/28/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ theone/theoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  theoneApp.swift
+//  theone
+//
+//  Created by nguyenlam on 4/28/22.
+//
+
+import SwiftUI
+
+@main
+struct theoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
